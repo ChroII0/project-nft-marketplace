fb8e006c8dd74837bc1f6cf59cf18e0c38441d77
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-08 22:18:31 +0700

    local testnet

--- client/src/util/contract.js
+++ client/src/util/contract.js
@@ -1,5 +1,5 @@
-export const addressSaturnBox = "0x18A3286d49Bc74b5e39A1E0C026B203122853d7C";
-export const addressSaturnMKP = "0x84fDD04Bc2DDAe9C8bB663d6AdDCEC84c6Ead46c";
+export const addressSaturnBox = "0x0Dab01a2445f3a51db8d928C5a4085e7362e75B1";
+export const addressSaturnMKP = "0x983B04051d3B7c8E493Ccdd8044E86b8e6e0D70b";
 
 export const abiSaturnBox = require('./abi/abiSaturnBox.json');
 export const abiSaturnMKP = require('./abi/abiSaturnMKP.json'); 
