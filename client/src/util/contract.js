947da31ee66e3e081bd6bdf310c827871b0fdfaf
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-09 00:42:07 +0700

    change contract

--- client/src/util/contract.js
+++ client/src/util/contract.js
@@ -1,5 +1,5 @@
-export const addressSaturnBox = "0x0964FC9b9E5b135063B64C3Dacc3FEdDD7a74F9F";
-export const addressSaturnMKP = "0xdf5d9A4e662F24dE668285836E328343Ed2Fb4aA";
+export const addressSaturnBox = "0x6A422A0C4183F50E54375Bd0B85c325b3810Fc85";
+export const addressSaturnMKP = "0x9a1aB61D7c125847aC91728f86Bf434C827Ad2C3";
 
 export const abiSaturnBox = require('./abi/abiSaturnBox.json');
 export const abiSaturnMKP = require('./abi/abiSaturnMKP.json'); 
