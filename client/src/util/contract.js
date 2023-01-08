a5c34d9c51605e0e6c16a930c56222127519f922
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-08 23:07:14 +0700

    withdraw

--- client/src/util/contract.js
+++ client/src/util/contract.js
@@ -1,5 +1,5 @@
-export const addressSaturnBox = "0xd00da2Fa9c179657A4F9996989B9a06354F59979";
-export const addressSaturnMKP = "0x8a46860Bba8Cc7bA5d109354623e7cd4D55dC238";
+export const addressSaturnBox = "0x116e63Be82A3Fe83769d21F9A8C8CfdFc5FB012B";
+export const addressSaturnMKP = "0xED567fa1584e321409A1171A333Ca49fC7c8D178";
 
 export const abiSaturnBox = require('./abi/abiSaturnBox.json');
 export const abiSaturnMKP = require('./abi/abiSaturnMKP.json'); 
