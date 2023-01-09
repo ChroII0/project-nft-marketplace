3d4091161081236f7e01d8b8d84d384590780dcf
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-09 08:36:09 +0700

    final local

--- client/src/util/contract.js
+++ client/src/util/contract.js
@@ -1,8 +1,8 @@
 // export const addressSaturnBox = "0x83957c709F7c6aaBdd6886430246Bb4215a70545";
 // export const addressSaturnMKP = "0x6B78dBb64FE42cAA4D7A67c21BcCe233A721fcFC"; goerli
 
-export const addressSaturnBox = "0x19b0D12DcA069fa80C8Be95D89B58ac245B22eEC";
-export const addressSaturnMKP = "0xF5a2FddEdFc40f9F9f79c09be0F712f2dE650f4C";
+export const addressSaturnBox = "0x08137820fB64F1faFA52BF56965F464b20a77B30";
+export const addressSaturnMKP = "0x175FF7eb0b5ABFab5991f9b72E35a144CE233251";
 
 export const abiSaturnBox = require('./abi/abiSaturnBox.json');
 export const abiSaturnMKP = require('./abi/abiSaturnMKP.json'); 
