707ccbb96f51520af6ee2667c46f8b1416169573
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-09 07:50:46 +0700

    goerli final

--- client/src/util/contract.js
+++ client/src/util/contract.js
@@ -1,5 +1,5 @@
-export const addressSaturnBox = "0x6A422A0C4183F50E54375Bd0B85c325b3810Fc85";
-export const addressSaturnMKP = "0x9a1aB61D7c125847aC91728f86Bf434C827Ad2C3";
+export const addressSaturnBox = "0x83957c709F7c6aaBdd6886430246Bb4215a70545";
+export const addressSaturnMKP = "0x6B78dBb64FE42cAA4D7A67c21BcCe233A721fcFC";
 
 export const abiSaturnBox = require('./abi/abiSaturnBox.json');
 export const abiSaturnMKP = require('./abi/abiSaturnMKP.json'); 
