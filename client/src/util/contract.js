bed3c70e5b036392246db53470ef1ccf7bd9f7b6
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-11 00:29:45 +0700

    update network

--- client/src/util/contract.js
+++ client/src/util/contract.js
@@ -1,8 +1,8 @@
-// export const addressSaturnBox = "0x83957c709F7c6aaBdd6886430246Bb4215a70545";
-// export const addressSaturnMKP = "0x6B78dBb64FE42cAA4D7A67c21BcCe233A721fcFC"; goerli
+export const addressSaturnBox = "0x83957c709F7c6aaBdd6886430246Bb4215a70545";
+export const addressSaturnMKP = "0x6B78dBb64FE42cAA4D7A67c21BcCe233A721fcFC"; //goerli
 
-export const addressSaturnBox = "0x08137820fB64F1faFA52BF56965F464b20a77B30";
-export const addressSaturnMKP = "0x175FF7eb0b5ABFab5991f9b72E35a144CE233251";
+// export const addressSaturnBox = "0x08137820fB64F1faFA52BF56965F464b20a77B30";
+// export const addressSaturnMKP = "0x175FF7eb0b5ABFab5991f9b72E35a144CE233251";
 
 export const abiSaturnBox = require('./abi/abiSaturnBox.json');
 export const abiSaturnMKP = require('./abi/abiSaturnMKP.json'); 
